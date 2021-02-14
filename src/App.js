0307b6fe59e7c19d4cfa12cb6b4665cf2900df6c
Justas Kuizinas <Justas_Kuizinas@epam.com>
2021-02-14 13:42:39 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
+import React, { PureComponent } from 'react';
 import './App.css';
+import ClassComp from './ClassComp';
+import PureComp from './PureComp';
+import FunctionalComp from './FunctionalComp';
+import CreateEl from './CreateElem';
 
 function App() {
+
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Hello World
       </header>
+      {CreateEl}
+      <ClassComp/>
+      <PureComp/>
+      <FunctionalComp/>
+ 
+      
     </div>
   );
 }
